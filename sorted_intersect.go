c1fa154d7b35dca98d65b344d5fd2557261f0164
Doug Turnbull <softwaredoug@gmail.com>
2024-12-07 10:16:40 -0500

    Add a sorted intersect with index into common header

--- sorted_intersect.go
+++ sorted_intersect.go
@@ -80,8 +80,6 @@ func profileCall(f intersectFunc, lhs []int64, rhs []int64, times int) (lhs_indi
     for i := 0; i < times; i++ {
         lhs_indices, rhs_indices = f(lhs, rhs)
     }
-    // fmt.Printf("lhs_indices: %v\n", lhs_indices[len(lhs_indices)-10:])
-    // fmt.Printf("rhs_indices: %v\n", lhs_indices[len(rhs_indices)-10:])
     return lhs_indices, rhs_indices
 }
 
@@ -100,41 +98,163 @@ func randArrs(seed, lengthLhs, lengthRhs int, maxVal int64) (lhs []int64, rhs []
 }
 
 
-func buildIndexMask(sorted_arr []int64, maxValue int64) int64 {
-    var bitmask int64
-    var currMin, currMax int64
-    var i int64
-
-    for i = 0; i < 64; i++ {
-        currMin = i * (maxValue / 64)
-        currMax = (i + 1) * (maxValue / 64)
-        for _, val := range sorted_arr {
-            if val >= currMin && val < currMax {
-                bitmask |= 1 << i
+func randArrsWithHeaders(seed, lengthLhs, lengthRhs int) (lhs []int64, rhs []int64) {
+    rand.Seed(int64(seed))
+    header := 0
+    value := 0
+    for i := 0; i < lengthLhs; i++ {
+        incrHeader := rand.Intn(10) == 0
+        if incrHeader {
+            header++
+            value = 0
+        }
+        lhs = append(lhs, int64(header << 32 | value))
+        value++
+        for j:=0; j < 10; j++ {
+            incrValue := rand.Intn(2) == 0
+            if incrValue {
+                value++
+            } else {
                 break
             }
         }
     }
-    return bitmask
+    header = 0
+    value = 0
+    for i := 0; i < lengthRhs; i++ {
+        incrHeader := rand.Intn(10) == 0
+        if incrHeader {
+            header++
+            value = 0
+        }
+        rhs = append(rhs, int64(header << 32 | value))
+        for j:=0; j < 10; j++ {
+            incrValue := rand.Intn(2) == 0
+            if incrValue {
+                value++
+            } else {
+                break
+            }
+        }
+    }
+    return lhs, rhs
 }
 
 
+func headerIndices(arr []int64) (headers []int64) {
+    lastHeader := -1
+    for i := 0; i < len(arr); i++ {
+        headerVal := int(arr[i] >> 32)
+        if headerVal != lastHeader {
+            value := int64(headerVal << 32) | int64(i)
+            headers = append(headers, value)
+            lastHeader = int(arr[i] >> 32)
+        }
+    }
+    return headers
+}
+
+
+func intersectWithHeaderMarks(lhs []int64, rhs []int64, lhsHeaders []int64, rhsHeaders []int64) (lhs_indices []int, rhs_indices []int) {
+
+    lhsHeaderIdx := 0
+    rhsHeaderIdx := 0
+
+    for lhsHeaderIdx < len(lhsHeaders) && rhsHeaderIdx < len(rhsHeaders) {
+        if (lhsHeaders[lhsHeaderIdx] >> 32) < (rhsHeaders[rhsHeaderIdx] >> 32) {
+            lhsHeaderIdx++
+        } else if (lhsHeaders[lhsHeaderIdx] >> 32) > (rhsHeaders[rhsHeaderIdx] >> 32) {
+            rhsHeaderIdx++
+        } else {
+            // get the index into lhs / rhs
+            lhsSliceStart := int(lhsHeaders[lhsHeaderIdx] & 0xFFFFFFFF)
+            rhsSliceStart := int(rhsHeaders[rhsHeaderIdx] & 0xFFFFFFFF)
+            lhsSliceNext := len(lhs)
+            rhsSliceNext := len(rhs)
+            if lhsHeaderIdx + 1 < len(lhsHeaders) {
+                lhsSliceNext = int(lhsHeaders[lhsHeaderIdx+1] & 0xFFFFFFFF)
+            } 
+            if rhsHeaderIdx + 1 < len(rhsHeaders) {
+                rhsSliceNext = int(rhsHeaders[rhsHeaderIdx+1] & 0xFFFFFFFF)
+            }
+
+            lhsSlice := lhs[lhsSliceStart:lhsSliceNext]
+            rhsSlice := rhs[rhsSliceStart:rhsSliceNext]
+            // two pointer intesrect
+            i, j := 0, 0
+            for i < len(lhsSlice) && j < len(rhsSlice) {
+                if lhsSlice[i] < rhsSlice[j] {
+                    i++
+                } else if lhsSlice[i] > rhsSlice[j] {
+                    j++
+                } else {
+                    lhs_indices = append(lhs_indices, i + lhsSliceStart)
+                    rhs_indices = append(rhs_indices, j + rhsSliceStart)
+                    i++
+                    j++
+                }
+            }
+            rhsHeaderIdx++
+            lhsHeaderIdx++
+        }
+    }
+    return lhs_indices, rhs_indices
+}
+
+
+func makeHeaderIntesectFn(
+    lhs []int64, rhs []int64, lhsHeaders []int64, rhsHeaders []int64,
+) intersectFunc {
+    return func(lhs []int64, rhs []int64) ([]int, []int) {
+        return intersectWithHeaderMarks(lhs, rhs, lhsHeaders, rhsHeaders)
+    }
+}
+
 func main() {
     fmt.Println("******************")
-    fmt.Println("Lobsided")
-    lhs, rhs := randArrs(42, 100, 1000000, 10000)
-    bitmaskLhs := buildIndexMask(lhs, 10000)
-    bitmaskRhs := buildIndexMask(rhs, 10000)
-    fmt.Printf("lhs bitmask: %0x\n", uint64(bitmaskLhs))
-    fmt.Printf("rhs bitmask: %0x\n", uint64(bitmaskRhs))
+    fmt.Println("Lobsided -- dense header")
+    // lhs, rhs := randArrs(42, 100, 1000000, maxVal)
+    lhs, rhs := randArrsWithHeaders(42, 100, 1000000)
+    lhsHeaders := headerIndices(lhs)
+    rhsHeaders := headerIndices(rhs)
+    headerIntersectFn := makeHeaderIntesectFn(lhs, rhs, lhsHeaders, rhsHeaders)
 
     profileCall(sortedIntersectNaive, lhs, rhs, 100)
     profileCall(sortedIntersectGalloping, lhs, rhs, 100)
+    profileCall(headerIntersectFn, lhs, rhs, 100)
    
     fmt.Println("******************")
-    fmt.Println("Even")
-    lhs, rhs = randArrs(42, 1000000, 1000000, 10000)
+    fmt.Println("Even -- dense header")
+    lhs, rhs = randArrsWithHeaders(42, 1000000, 1000000)
+    lhsHeaders = headerIndices(lhs)
+    rhsHeaders = headerIndices(rhs)
+    headerIntersectFn = makeHeaderIntesectFn(lhs, rhs, lhsHeaders, rhsHeaders)
 
     profileCall(sortedIntersectNaive, lhs, rhs, 100)
     profileCall(sortedIntersectGalloping, lhs, rhs, 100)
+    profileCall(headerIntersectFn, lhs, rhs, 100)
+    
+    fmt.Println("******************")
+    fmt.Println("Lobsided -- truly random")
+    maxVal := int64(10000)
+    lhs, rhs = randArrs(42, 100, 1000000, maxVal)
+    lhsHeaders = headerIndices(lhs)
+    rhsHeaders = headerIndices(rhs)
+    headerIntersectFn = makeHeaderIntesectFn(lhs, rhs, lhsHeaders, rhsHeaders)
+
+    profileCall(sortedIntersectNaive, lhs, rhs, 100)
+    profileCall(sortedIntersectGalloping, lhs, rhs, 100)
+    profileCall(headerIntersectFn, lhs, rhs, 100)
+    
+    fmt.Println("******************")
+    fmt.Println("Even -- truly random")
+    lhs, rhs = randArrs(42, 100000, 1000000, maxVal)
+    lhsHeaders = headerIndices(lhs)
+    rhsHeaders = headerIndices(rhs)
+    headerIntersectFn = makeHeaderIntesectFn(lhs, rhs, lhsHeaders, rhsHeaders)
+
+    profileCall(sortedIntersectNaive, lhs, rhs, 100)
+    profileCall(sortedIntersectGalloping, lhs, rhs, 100)
+    profileCall(headerIntersectFn, lhs, rhs, 100)
+   
 }
